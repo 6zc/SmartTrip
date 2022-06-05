0f81cb62e5a4f2ddf60dd1ab2f11168e1ee2cc04
zichang.liu <>
2022-06-05 16:12:39 +0800

    Delete duplicate screen

--- App/App.js
+++ App/App.js
@@ -120,20 +120,6 @@ const App = () => {
             ),
           }}
         />
-        <Tabs.Screen
-          name="Login"
-          component={LoginPage}
-          options={{
-            tabBarIcon: ({focused, color, size}) => (
-              <User
-                name="user"
-                size={34}
-                color={focused ? color : '#4da4dd'}
-                focused={focused}
-              />
-            ),
-          }}
-        />
       </Tabs.Navigator>
     </NavigationContainer>
   );
