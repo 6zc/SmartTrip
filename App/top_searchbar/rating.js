0ace3c4e93ae18fe5e3456403be8ee1948862233
zichang.liu <>
2022-07-02 10:36:54 +0800

    Fix Lint

--- App/top_searchbar/rating.js
+++ App/top_searchbar/rating.js
@@ -2,15 +2,15 @@ import React from "react";
 // import { StyleSheet, View } from "react-native";
 import Ionicon from "react-native-vector-icons/Ionicons";
 
-const Rating = props => {
+const Rating = (props) => {
   const { rate } = props;
-  return(
-    rate===1?
-    <Ionicon name="star" size={18} color="#ffd700"/>:
-    (rate===0.5?<Ionicon name="star-half" size={18} color="#ffd700"/>:
-    <Ionicon name="star" size={18} color="#cecece"/>
-    )
-  )
-}
+  return rate === 1 ? (
+    <Ionicon name="star" size={18} color="#ffd700" />
+  ) : rate === 0.5 ? (
+    <Ionicon name="star-half" size={18} color="#ffd700" />
+  ) : (
+    <Ionicon name="star" size={18} color="#cecece" />
+  );
+};
 
-export default Rating
+export default Rating;
--- App/top_searchbar/result_item.js
+++ App/top_searchbar/result_item.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import {StyleSheet,View,Text, TouchableOpacity, Image, Alert} from 'react-native';
-import Linking from '../utils/linking';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import Linking from "../utils/linking";
 import Ionicon from "react-native-vector-icons/Ionicons";
-import Rating from './rating';
-import { assertScalarType } from 'graphql';
-import {Svg, Image as ImageSvg} from 'react-native-svg';
+import Rating from "./rating";
+import { assertScalarType } from "graphql";
+import { Svg, Image as ImageSvg } from "react-native-svg";
 
-
-
-const Item = props => {
+const Item = (props) => {
   // TEMPORARY DATA
-  const rating = Math.random()*5
-  const liked = Math.random()>0.5
+  const rating = Math.random() * 5;
+  const liked = Math.random() > 0.5;
 
-  const {item} = props
-  const {title, image, location, type} = item
+  const { item } = props;
+  const { title, image, location, type } = item;
   const coordinate = {
-    longitude: location.lon+0.0049,
-    latitude: location.lat-0.0028,
-  }
-  let rateArray = new Array(5).fill(0)
-  rateArray = rateArray.map((_item, index)=>{
-    if(index<Math.floor(rating)) return 1
-    else if(index>rating) return 0
-    else if(index+1>rating) return 0.5
-  })
+    longitude: location.lon + 0.0049,
+    latitude: location.lat - 0.0028,
+  };
+  let rateArray = new Array(5).fill(0);
+  rateArray = rateArray.map((_item, index) => {
+    if (index < Math.floor(rating)) return 1;
+    else if (index > rating) return 0;
+    else if (index + 1 > rating) return 0.5;
+  });
   return (
     <View style={styles.container}>
       <View style={styles.imageWrapper}>
         <Svg style={styles.imageWrapper.image}>
           <ImageSvg
-            width={'100%'} 
-            height={'100%'}
+            width={"100%"}
+            height={"100%"}
             preserveAspectRatio="xMidYMid slice"
-            href={{ uri: image.url}}
+            href={{ uri: image.url }}
           />
         </Svg>
         {/* <Image style={styles.imageWrapper.image} source={image} /> */}
@@ -44,116 +49,116 @@ const Item = props => {
           <Text style={styles.title.type}>{type}</Text>
         </View>
         <View style={styles.starWrapper}>
-          {
-            rateArray.map((item, index) =>
-              <Rating 
-                rate={item}
-                key={index}
-              />
-            )
-          }
+          {rateArray.map((item, index) => (
+            <Rating rate={item} key={index} />
+          ))}
         </View>
       </View>
       <View style={styles.backOpacity}></View>
-      <TouchableOpacity 
+      <TouchableOpacity
         style={styles.navi}
-        onPress={() => Linking.link2map(...Object.values(coordinate), title)}>
-        <Ionicon name="arrow-forward-circle-outline" size={45} color="#ffffff" />
+        onPress={() => Linking.link2map(...Object.values(coordinate), title)}
+      >
+        <Ionicon
+          name="arrow-forward-circle-outline"
+          size={45}
+          color="#ffffff"
+        />
       </TouchableOpacity>
-      <TouchableOpacity 
+      <TouchableOpacity
         style={styles.heart}
-        onPress={() => Alert.alert('Added to Favorite')}>
-        {
-          liked?
+        onPress={() => Alert.alert("Added to Favorite")}
+      >
+        {liked ? (
           <Ionicon name="heart" size={40} color="#ff4040" />
-          :
+        ) : (
           <Ionicon name="heart-outline" size={40} color="#ffffff" />
-        }
+        )}
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  starWrapper:{
-    zIndex:2,
+  starWrapper: {
+    zIndex: 2,
     height: 30,
     width: 110,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    left:  10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    left: 10,
   },
-  backOpacity:{
-    position: 'absolute',
+  backOpacity: {
+    position: "absolute",
     left: 0,
     top: 0,
     zIndex: -1,
     opacity: 0.7,
-    backgroundColor: '#b5b5b5',
+    backgroundColor: "#b5b5b5",
     width: 320,
-    height: 150
+    height: 150,
   },
-  imageWrapper:{
-    width:130,
-    height:150,
-    overflow: 'hidden',
+  imageWrapper: {
+    width: 130,
+    height: 150,
+    overflow: "hidden",
     left: -10,
-    image:{
+    image: {
       height: 150,
       width: 150,
       left: -10,
       zIndex: 2,
     },
   },
-  navi:{
-    position: 'absolute',
+  navi: {
+    position: "absolute",
     right: 4,
     bottom: 2,
-    zIndex: 2
+    zIndex: 2,
   },
-  heart:{
-    position: 'absolute',
+  heart: {
+    position: "absolute",
     right: 4,
     top: 2,
-    zIndex: 2
+    zIndex: 2,
   },
-  title:{
-    borderColor: '#000000',
+  title: {
+    borderColor: "#000000",
     width: 200,
-    flexDirection: 'column',
+    flexDirection: "column",
     left: -10,
-    alignSelf: 'stretch',
-    justifyContent: 'space-between',
-    text:{
-      fontSize:18,
-      fontWeight:'bold',
+    alignSelf: "stretch",
+    justifyContent: "space-between",
+    text: {
+      fontSize: 18,
+      fontWeight: "bold",
       lineHeight: 18,
       width: 150,
       left: 8,
       top: 8,
-      color: '#ffffff',
+      color: "#ffffff",
     },
-    type:{
-      fontSize:15,
-      fontWeight:'bold',
+    type: {
+      fontSize: 15,
+      fontWeight: "bold",
       lineHeight: 25,
       width: 150,
       left: 8,
       top: 5,
-      color: '#cecece',
+      color: "#cecece",
     },
   },
   container: {
     marginTop: 15,
-    overflow: 'hidden',
+    overflow: "hidden",
     // backgroundColor: '#ffffff',
     height: 150,
     width: 320,
     marginBottom: 15,
     borderRadius: 10,
-    alignItems: 'flex-start',
-    flexDirection: 'row',
+    alignItems: "flex-start",
+    flexDirection: "row",
   },
-})
+});
 
-export default Item
+export default Item;
